a3f8ed1a2cc2f1d383d99d3c31093070a2ec549a
unknown <khnl.prem@gmail.com>
2019-03-08 16:32:23 +0545

    changes file

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-﻿//[custom Javascript]
-//Project:	Aero - Responsive Bootstrap 4 Template
-//Version:  1.0
-//Last change:  15/12/2019
-//Primary use:	Aero - Responsive Bootstrap 4 Template
-//should be included in all pages. It controls some layout
+﻿
 $(function() {
     "use strict";
     initSparkline();
